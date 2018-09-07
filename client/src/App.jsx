a9d7ebf3951887457f9f08a98fe6cac8dcd32167
github username <github email address>
2018-09-06 23:36:58 -0400

    clearer syntax

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import * as actions from './store/actions';
+import { fetchUser } from './store/actions';
 
 class App extends Component {
     componentDidMount() {
@@ -19,5 +19,5 @@ class App extends Component {
 
 export default connect(
     null,
-    actions
+    { fetchUser }
 )(App);
