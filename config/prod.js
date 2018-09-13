e9a1e324c122cce450112dfd69caf41394b8ac97
github username <github email address>
2018-09-12 23:18:24 -0400

    .

--- config/prod.js
+++ config/prod.js
@@ -4,6 +4,3 @@ module.exports = {
     mongoDBConnectionURI: process.env.MONGO_DB_CONNECTION_STRING,
     cookieKey: process.env.COOKIE_KEY,
 };
-
-// googleProdId = 399047058783-rraaofko6n5vc3s5obc3012i9ljut1pk.apps.googleusercontent.com
-// googleProdSecret = I_RzK3BQ66XXANAC7ZKjDjnH
